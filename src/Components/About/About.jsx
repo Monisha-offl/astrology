b38a98999ed1fdc797c0a27638105efbd7df636a
Monisha-Off <nandhinikaruppasamy21@gmail.com>
2024-11-02 15:34:15 +0530

    updated

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./About.css"
+
+const About = () => {
+  return (
+    <>
+     <div className='about'>
+        <section className='about-intro-wholediv'>
+            <div className='aboutpage-image'>
+
+            </div>
+            <div className='about-inro-content'>
+                <p>Home/ About</p>
+            </div>
+        </section>
+        </div> 
+    </>
+  )
+}
+
+export default About
